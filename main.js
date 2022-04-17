42e493894064397eb6387e1b2c5e6c6f3342291b
sarazaToussaint <tmsaraza@gmail.com>
2022-04-18 00:33:04 +0200

    Create objects to and function to make the featured speaker section dynamic

--- main.js
+++ main.js
@@ -19,3 +19,66 @@ window.addEventListener('resize', () => {
     hamburger.classList.remove('hidden');
   }
 });
+
+const speakerList = [{
+  imageLink: './components/peak.png',
+  title: 'Robert Pong',
+  description: 'Lorem ipsum dolor.',
+  fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+},
+
+{
+  imageLink: './components/peak.png',
+  title: 'Denis Lapo',
+  description: 'Lorem ipsum dolor.',
+  fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+},
+
+{
+  imageLink: './components/speaker4.jpg',
+  title: 'Dan Levis',
+  description: 'Lorem ipsum dolor.',
+  fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+},
+
+{
+  imageLink: './components/speaker1.jpg',
+  title: 'Alicia Keys',
+  description: 'Lorem ipsum dolor.',
+  fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+},
+
+{
+  imageLink: './components/speaker2.jpg',
+  title: 'Toussaint Saraza',
+  description: 'Lorem ipsum dolor.',
+  fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+},
+
+{
+  imageLink: './components/speaker3.jpg',
+  title: 'Kevin Brown',
+  description: 'Lorem ipsum dolor.',
+  fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+}];
+
+const cardContainer = document.querySelector('.featured-speakers');
+
+const speakers = () => {
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < speakerList.length; i++) {
+    cardContainer.innerHTML += `<div class="speakers">
+      <img id="speaker1" class="speak-img" src="${speakerList[i].imageLink}" alt="${speakerList[i].title}">
+      <div class="speak-element">
+        <h3 id="title">${speakerList[i].title}</h3>
+        <h6 id="subTitle">${speakerList[i].description}
+        </h6>
+        <p id="paragraph">
+          ${speakerList[i].fullDescription}
+        </p>
+      </div>
+  </div>`;
+  }
+};
+
+speakers();
