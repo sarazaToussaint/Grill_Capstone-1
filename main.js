8a0c8d3d815c7d9fd1496461e5f5fa623a4446d0
sarazaToussaint <tmsaraza@gmail.com>
2023-06-01 22:08:44 +0200

    Move the main.js file in components

--- main.js
+++ main.js
@@ -21,42 +21,42 @@ window.addEventListener('resize', () => {
 });
 
 const speakerList = [{
-  imageLink: './components/peak.png',
+  imageLink: './images/speaker4.jpg',
   title: 'Robert Pong',
   description: 'Lorem ipsum dolor.',
   fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
 },
 
 {
-  imageLink: './components/peak.png',
+  imageLink: './images/speaker4.jpg',
   title: 'Denis Lapo',
   description: 'Lorem ipsum dolor.',
   fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
 },
 
 {
-  imageLink: './components/speaker4.jpg',
+  imageLink: './images/speaker4.jpg',
   title: 'Dan Levis',
   description: 'Lorem ipsum dolor.',
   fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
 },
 
 {
-  imageLink: './components/speaker1.jpg',
+  imageLink: './images/speaker1.jpg',
   title: 'Alicia Keys',
   description: 'Lorem ipsum dolor.',
   fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
 },
 
 {
-  imageLink: './components/speaker2.jpg',
-  title: 'Toussaint Saraza',
+  imageLink: './images/speaker2.jpg',
+  title: 'John Don',
   description: 'Lorem ipsum dolor.',
   fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
 },
 
 {
-  imageLink: './components/speaker3.jpg',
+  imageLink: './images/speaker3.jpg',
   title: 'Kevin Brown',
   description: 'Lorem ipsum dolor.',
   fullDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
@@ -68,7 +68,9 @@ const speakers = () => {
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < speakerList.length; i++) {
     cardContainer.innerHTML += `<div class="speakers">
-      <img id="speaker1" class="speak-img" src="${speakerList[i].imageLink}" alt="${speakerList[i].title}">
+      <div class="speaker-img">
+        <img id="speaker1" class="speak-img" src="${speakerList[i].imageLink}" alt="${speakerList[i].title}">
+      </div>
       <div class="speak-element">
         <h3 id="title">${speakerList[i].title}</h3>
         <h6 id="subTitle">${speakerList[i].description}
